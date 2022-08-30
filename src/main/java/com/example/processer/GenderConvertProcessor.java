e97dae1997a7857cc3be945438c01207e154bad2
erikamikami <g.erika3up@gmail.com>
2022-08-30 16:57:17 +0900

    プロセッサ作成

--- src/main/java/com/example/processer/GenderConvertProcessor.java
+++ src/main/java/com/example/processer/GenderConvertProcessor.java
@@ -1,5 +1,31 @@
 package com.example.processer;
 
-public class GenderConvertProcessor {
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import com.example.domain.Employee;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component("GenderConvertProcessor")
+@StepScope
+@Slf4j
+public class GenderConvertProcessor implements ItemProcessor<Employee, Employee>{
+	
+	/**
+	 * 性別の文字列を数値に変換する
+	 *
+	 */
+	@Override
+	public Employee process(Employee item) throws Exception {
+		try {
+			item.convertGenderStringToInt();
+		} catch (Exception e) {
+			log.warn(e.getMessage(), e);
+			return null;
+		}
+		return item;
+	}
 
 }
